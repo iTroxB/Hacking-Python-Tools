a383d04ae7e55a75236beb879af3f0147563c5e3
iTrox <143133090+iTroxB@users.noreply.github.com>
2023-11-10 19:00:31 -0300

    Create client.py

---
+++ TCP server/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = '192.168.0.7'
+host = socket.gethostname()
+port = 444
+
+clientsocket.connect(('192.168.1.104', port))
+
+message = clientsocket.recv(1024)
+
+print(message.decode('ascii'))
